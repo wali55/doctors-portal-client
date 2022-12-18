8c4601ef915c9ef0f83d82c4321813d5a3ce8de7
wali55 <walisantunu@gmail.com>
2022-12-18 18:34:53 +0600

    navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Shared/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
